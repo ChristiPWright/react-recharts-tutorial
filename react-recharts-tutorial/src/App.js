5369cfacae61ec2414b022505fefe08b1f7901b6
Christi <christi@studylog.com>
2023-01-27 13:49:01 -0500

    WIP adding line chart for bitcoin price history

--- react-recharts-tutorial/src/App.js
+++ react-recharts-tutorial/src/App.js
@@ -1,24 +1,34 @@
 import { useEffect, useState } from "react";
-import Chart from "./Chart";
+import BarChart from "./barChart";
+import LineChart from "./lineChart";
 
 export default function App() {
-  const [data, setdata] = useState();
+  const [assetData, setAssetData] = useState();
+  const [bitcoinHistoryData, setBitcoinHistoryData] = useState();
 
-  const fetchDatas = async () => {
+  const fetchAssetDatas = async () => {
     const res = await fetch("https://api.coincap.io/v2/assets/?limit=20");
-    const data = await res.json();
-    console.log(data);
-    setdata(data?.data);
+    const assetData = await res.json();
+    console.log(assetData);
+    setAssetData(assetData?.data);
+  };
+  const fetchBitcoinHistoryDatas = async () => {
+    const res = await fetch("https://api.coincap.io/v2/assets/bitcoin/history?interval=d1"); //TODO: interatively make call & render linechart or dropdown to select id for one line chart
+    const bitcoinHistoryData = await res.json();
+    console.log(bitcoinHistoryData);
+    setBitcoinHistoryData(bitcoinHistoryData?.data);
   };
 
   useEffect(() => {
-    fetchDatas();
+    fetchAssetDatas();
+    fetchBitcoinHistoryDatas()
   }, []);
 
   return (
     <div className="App">
       How to use Recharts with React
-      <Chart data={data} />
+      <BarChart data={assetData} />
+      <LineChart data={bitcoinHistoryData} />
     </div>
   );
 }
---
+++ react-recharts-tutorial/src/lineChart.js
@@ -0,0 +1,36 @@
+import {
+    LineChart, 
+    Line, 
+    XAxis, 
+    YAxis, 
+    CartesianGrid, 
+    Tooltip, 
+    Legend, 
+    ResponsiveContainer
+  } from "recharts";
+
+export default function Chart({ data }) {
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+            >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="priceUsd" stroke="#8884d8" />
+            <Line type="monotone" dataKey="date" stroke="#82ca9d" />
+            </LineChart>
+        </ResponsiveContainer>
+    );
+}
